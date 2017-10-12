0c05848d1175cd96b8aa3be4972729544e34b7e6
wuwf <272551766@qq.com>
2017-10-12 11:06:30 +0800

    加入熔断

---
+++ src/main/java/com/tianyalei/testzuul/fallback/UserServiceFallbackProvider.java
@@ -0,0 +1,68 @@
+package com.tianyalei.testzuul.fallback;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * Created by wuweifeng on 2017/10/12.
+ */
+@Component
+public class UserServiceFallbackProvider implements ZuulFallbackProvider {
+
+    @Value("${zuul.routes.api-user.serviceId}")
+    private String SERVICEID;
+
+
+    @Override
+    public String getRoute() {
+        return SERVICEID;
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("Service-user不可用".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mt = new MediaType("application", "json", Charset.forName("UTF-8"));
+                headers.setContentType(mt);
+                return headers;
+            }
+        };
+    }
+}
